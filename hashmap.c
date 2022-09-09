72dcc27d44438c8d2a90b1a2523ba7ede3befe5b
Replit user <>
2022-09-09 12:59:20 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -34,45 +34,96 @@ long hash( char * key, long capacity) {
 
 int is_equal(void* key1, void* key2){
     if(key1==NULL || key2==NULL) return 0;
-    if(strcmp((char*)key1,(char*)key2) == 0) return 1;
-    return 0;
+    if(strcmp((char*)key1,(char*)key2) == 0) return 0;
+    return 1;
 }
 
 
+//--------------------------------------------------------------------------------------------
+
+
+
+HashMap * createMap(long capacity) {
+  //1.- Implemente la función *createMap* en el archivo hashmap.c. Esta función crea una variable     de tipo HashMap, inicializa el arreglo de buckets con casillas nulas, inicializa el resto de       variables y retorna el mapa. Inicialice el índice current a -1.
+    HashMap *map = (HashMap *)calloc(1,sizeof(HashMap));
+    map->buckets = (Pair **)calloc(capacity,sizeof(Pair));
+    map->size = 0;
+    map->current = -1;
+  
+    return map;
+}
+
 void insertMap(HashMap * map, char * key, void * value) {
+//2.- Implemente la función void insertMap(HashMap * map, char * key, void * value). Esta función inserta un nuevo dato (key,value) en el mapa y actualiza el índice current a esa posición.
+//Recuerde que para insertar un par (clave,valor) debe:
+
+//a - Aplicar la función hash a la clave para obtener la posición donde debería insertar el nuevo par
+
+//b - Si la casilla se encuentra ocupada, avance hasta una casilla disponible (*método de resolución de colisiones*). Una casilla disponible es una casilla nula, pero también una que tenga un par inválido (key==NULL).
+
+//c - Ingrese el par en la casilla que encontró.
+
+//No inserte claves repetidas. 
+//Recuerde que el arreglo es **circular**.
+//Recuerde actualizar la variable size.
 
 
+
+  
+return;
 }
 
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
+//6.- Implemente la función void enlarge(HashMap * map). Esta función agranda la capacidad del arreglo buckets y reubica todos sus elementos. Para hacerlo es recomendable mantener referenciado el arreglo *actual/antiguo* de la tabla con un puntero auxiliar. Luego, los valores de la tabla se reinicializan con un nuevo arreglo con el **doble de capacidad**. Por último los elementos del arreglo antiguo se insertan en el mapa *vacío* con el método *insertMap*.
+//Puede seguir los siguientes pasos:
 
+//a - Cree una variable auxiliar de tipo Pair** para matener el arreglo map->buckets (*old_buckets*);
 
+//b - Duplique el valor de la variable capacity.
+
+//c - Asigne a map->buckets un nuevo arreglo con la nueva capacidad.
+
+//d - Inicialice size a 0.
+
+//e - Inserte los elementos del arreglo *old_buckets* en el mapa (use la función insertMap que ya implementó).
+return;
 }
 
 
-HashMap * createMap(long capacity) {
 
-    return NULL;
-}
 
 void eraseMap(HashMap * map,  char * key) {    
+//4.- Implemente la función void eraseMap(HashMap * map,  char * key). Está función elimina el dato correspondiente a la clave key. Para hacerlo debe buscar el dato y luego *marcarlo* para que no sea válido.
+//**No elimine el par**, sólo invalídelo asignando NULL a la clave (pair->key=NULL).
+//Recuerde actualizar la variable size.
 
-
+  return;
 }
 
 Pair * searchMap(HashMap * map,  char * key) {   
+  //3.- Implemente la función Pair * searchMap(HashMap * map,  char * key), la cual retorna el **Pair** asociado a la clave ingresada. 
+//Recuerde que para buscar el par debe:
+
+//a - Usar la función hash para obtener la posición donde puede encontrarse el par con la clave
+
+//b - Si la clave no se encuentra avance hasta encontrarla (*método de resolución de colisiones*)
+
+//c - Si llega a una casilla nula, retorne NULL inmediatamente (no siga avanzando, la clave no está)
+
+//Recuerde actualizar el índice current a la posición encontrada.
+//Recuerde que el arreglo es **circular**.
 
 
     return NULL;
 }
 
 Pair * firstMap(HashMap * map) {
-
+//Pair * firstMap(HashMap * map) retorna el primer **Pair** válido del arreglo buckets. Recuerde actualizar el índice.
     return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
-
+//Pair * nextMap(HashMap * map) retorna el siguiente **Pair** del arreglo buckets a partir índice current. Recuerde actualizar el índice.
     return NULL;
 }
