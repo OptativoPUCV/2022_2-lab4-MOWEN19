bc78319adb207d7d7ea5a8eef35036e2cbe781c6
Replit user <>
2022-09-10 18:30:52 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -197,6 +197,12 @@ void enlarge(HashMap * map) {
 //d - Inicialice size a 0.
 
 //e - Inserte los elementos del arreglo *old_buckets* en el mapa (use la función insertMap que ya implementó).
+  //Pair ** oldBuckets = createMap(map->capacity*2);
+  //map->capacity *= 2;
+  // for(int i = 0; i < map->capacity; i++){
+    
   
+     
+  // }
   return;
 }
