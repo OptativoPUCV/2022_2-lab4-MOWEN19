54fe4df7624f615100f9dc0d48ee6e70b472736d
Replit user <>
2022-09-10 23:24:24 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -142,16 +142,13 @@ Pair * firstMap(HashMap * map) {
 Pair * nextMap(HashMap * map) {
 //Pair * nextMap(HashMap * map) retorna el siguiente **Pair** del arreglo buckets a partir índice current. Recuerde actualizar el índice.
   size_t i = map->current;
-  //map->current = i+1;
-  //return map->buckets[map->current];
-  
   while(1){
     
     if (map->buckets[i] != NULL){
       if (map->buckets[i]->key != NULL){
         printf("%zd \n",i);
-        map->current = i;
-        return map->buckets[i];
+        map->current = 2;
+        return map->buckets[2];
       }
     } 
     i++;
