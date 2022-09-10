c966d1176c04a73cf21cf0a113100260db6f2ce6
Replit user <>
2022-09-10 23:03:46 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -56,17 +56,8 @@ HashMap * createMap(long capacity) {
 
 void insertMap(HashMap * map, char * key, void * value) {
 //2.- Implemente la función void insertMap(HashMap * map, char * key, void * value). Esta función inserta un nuevo dato (key,value) en el mapa y actualiza el índice current a esa posición.
-//Recuerde que para insertar un par (clave,valor) debe:
 
-//a - Aplicar la función hash a la clave para obtener la posición donde debería insertar el nuevo par
 
-//b - Si la casilla se encuentra ocupada, avance hasta una casilla disponible (*método de resolución de colisiones*). Una casilla disponible es una casilla nula, pero también una que tenga un par inválido (key==NULL).
-
-//c - Ingrese el par en la casilla que encontró.
-
-//No inserte claves repetidas. 
-//Recuerde que el arreglo es **circular**.
-//Recuerde actualizar la variable size.
   Pair *newPair = createPair(key,value);
   size_t i = hash(key,map->capacity);//size_t == long long
   while(1){
@@ -85,16 +76,7 @@ void insertMap(HashMap * map, char * key, void * value) {
 
 Pair * searchMap(HashMap * map,  char * key) {   
   //3.- Implemente la función Pair * searchMap(HashMap * map,  char * key), la cual retorna el **Pair** asociado a la clave ingresada. 
-//Recuerde que para buscar el par debe:
 
-//a - Usar la función hash para obtener la posición donde puede encontrarse el par con la clave
-
-//b - Si la clave no se encuentra avance hasta encontrarla (*método de resolución de colisiones*)
-
-//c - Si llega a una casilla nula, retorne NULL inmediatamente (no siga avanzando, la clave no está)
-
-//Recuerde actualizar el índice current a la posición encontrada.
-//Recuerde que el arreglo es **circular**.
   size_t i = hash(key,map->capacity);
   size_t cont= 0;
   while(1){
@@ -160,24 +142,8 @@ Pair * firstMap(HashMap * map) {
 Pair * nextMap(HashMap * map) {
 //Pair * nextMap(HashMap * map) retorna el siguiente **Pair** del arreglo buckets a partir índice current. Recuerde actualizar el índice.
   size_t i = map->current;
-  size_t cont = 0;
-  while(1){
-    if (map->buckets[i]!=NULL){
-      if (map->buckets[i]->key != NULL){
-        map->current = i;
-        return map->buckets[i];
-      }
-    } 
-    i++;
-    cont++;
-    if (i == map->capacity){
-      return NULL;
-      
-    }
-    if (cont == map->capacity){
-        return NULL;
-    }
-  }
+  map->current = i+1;
+  return map->buckets[map->current];
 }
 
 
