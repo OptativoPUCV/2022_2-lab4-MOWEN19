a72c77a5be3777dfc29823d3b686049b1565f65e
Replit user <>
2022-09-11 18:06:36 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -171,9 +171,9 @@ void enlarge(HashMap * map) {
 //e - Inserte los elementos del arreglo *old_buckets* en el mapa (use la función insertMap que ya implementó).
   HashMap * newMap = createMap(map->capacity*2)  ;
   size_t i = 0;
-  //size_t oldMapCapacity = map->capacity;
+  size_t oldMapCapacity = map->capacity;
   while(1){
-    //size_t indice = hash(map->buckets[i]->key,oldMapCapacity);
+    size_t indice = hash(map->buckets[i]->key,oldMapCapacity);
     insertMap(newMap, map->buckets[i]->key, map->buckets[i]->value);
     i++;
     if (i == newMap->capacity){return;}
