fdf1b7df8be67917256906ee00a98ed9160a7cc4
Replit user <>
2022-09-11 19:42:42 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -174,6 +174,8 @@ void enlarge(HashMap * map) {
   Pair** oldBuckets = map->buckets;
   map->capacity =map->capacity*2;
   map->buckets = (Pair**)calloc(map->capacity,sizeof(Pair));
+
+  
   while(1){
     if(oldBuckets[i] != NULL){
       insertMap(map,oldBuckets[i]->key,oldBuckets[i]->value);
@@ -185,5 +187,5 @@ void enlarge(HashMap * map) {
     
   }
 
-  return;
+  //return;
 }
